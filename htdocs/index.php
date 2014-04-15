f0c476833596283d6318c36bd7da9f6d25672cae
Justin Manley <manleyjster@gmail.com>
2014-04-15 00:29:41 -0500

    Add Business of Style event info to 'Upcoming Events' featurette on homepage.

--- htdocs/index.php
+++ htdocs/index.php
@@ -40,8 +40,13 @@ echo pageHeader($page); ?>
              <a class="twitter-timeline" href="https://twitter.com/ChicagoCIE" data-widget-id="455494365406695424">Tweets by @ChicagoCIE</a>
             <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
 		</div>
-		<div class="featurette gastro-conference">
+		<div class="featurette" id="upcoming-events">
 			<h3>Upcoming events</h3>
+			<p>
+				<strong>Saturday, April 31</strong>
+				<br />
+				Conference: The Business of Style
+			</p>
 		</div>
 	</div>
 </div>
